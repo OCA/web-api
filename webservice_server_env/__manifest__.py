5be40249854a94fee6bade936eca184914ad80a1
Daniel Reis <dreis@opensourceintegrators.com>
2024-04-28 11:41:49 +0100

    [ADD] webservice_server_env: make server_environment an optional dependency

---
+++ webservice_server_env/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Creu Blanca
+# Copyright 2022 Camptocamp SA
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "WebService Server Environment",
+    "summary": "Use Server Environment feature to manage Webservice configs",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "development_status": "Production/Stable",
+    "maintainers": ["etobella"],
+    "author": "Creu Blanca, Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web-api",
+    "depends": ["webservice", "server_environment"],
+    "auto_install": True,
+}
