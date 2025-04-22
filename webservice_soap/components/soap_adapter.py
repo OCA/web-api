01f6c8a546e99d95e9dacf85d44470ab8bb17722
Hector Vior <hector.villarreal@forgeflow.com>
2025-04-22 17:45:27 +0200

    fixup! [ADD] webservice_soap

--- webservice_soap/components/soap_adapter.py
+++ webservice_soap/components/soap_adapter.py
@@ -5,6 +5,8 @@ from requests.auth import HTTPBasicAuth
 from zeep import Client, Settings
 from zeep.transports import Transport
 
+from odoo import _
+from odoo.exceptions import ValidationError
 from odoo.addons.component.core import Component
 
 _logger = logging.getLogger(__name__)
@@ -15,33 +17,48 @@ class SoapAdapter(Component):
     _inherit = "base.webservice.adapter"
     _webservice_protocol = "soap"
 
-    def _configure_client(self):
-        settings = Settings(
+    def _get_settings(self):
+        return Settings(
             strict=False,
             xml_huge_tree=True,
         )
-        session = Session()
-        self._session_auth(session)
 
+    def _get_session(self):
+        if self.collection.auth_type == "user_pwd":
+            if not self.collection.username and not self.collection.password:
+                raise ValidationError(_("Missing username and/or password"))
+            session = Session()
+            session.auth = HTTPBasicAuth(
+                self.collection.username,
+                self.collection.password
+            )
+            return session
+        return None
+
+    def _prepare_client(self):
+        client_config_dict = {
+            "wsdl": self.collection.url,
+        }
+        settings = self._get_settings()
+        if settings:
+            client_config_dict['settings'] = settings
+        session = self._get_session()
+        if session:
+            client_config_dict['transport'] = Transport(session=session)
+        if self.collection.soap_service_name:
+            client_config_dict['service_name'] = self.collection.soap_service_name
+        if self.collection.soap_port_name:
+            client_config_dict['port_name'] = self.collection.soap_port_name
+        return client_config_dict
+
+    def _get_client(self):
         return Client(
-            wsdl=self.collection.url,
-            transport=Transport(session=session),
-            settings=settings,
-            service_name=self.collection.soap_service_name,
-            port_name=self.collection.soap_port_name,
+            **self._prepare_client()
         )
 
-    def _session_auth(self, session):
-        if self.collection.auth_type == "user_pwd":
-            session.auth = HTTPBasicAuth(
-                self.collection.username, self.collection.password
-            )
-        elif self.collection.auth_type == "api_key":
-            session.headers.update(
-                {self.collection.api_key_header: self.collection.api_key}
-            )
-
     def __getattr__(self, attr):
-        client = self._configure_client()
+        if getattr(self, attr):
+            return getattr(self, attr)
+        client = self._get_client()
         service = client.service
         return getattr(service, attr)
