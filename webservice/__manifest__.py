217717a07d45bfab8880306ec6cc9fa1e869a621
Maksym Yankin <yankinmk@gmail.com>
2026-07-09 17:36:12 +0300

    [ADD] webservice_environment: make server_environment an optional dependency

--- webservice/__manifest__.py
+++ webservice/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "WebService",
     "summary": """Defines webservice abstract definition to be used generally""",
-    "version": "19.0.1.1.2",
+    "version": "19.0.1.1.3",
     "license": "AGPL-3",
     "development_status": "Production/Stable",
     "maintainers": ["etobella"],
