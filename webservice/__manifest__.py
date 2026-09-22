a7c57f7aeda98519d2ad661c10c182a6d6a80d3e
Simone Orsi <simahawk@gmail.com>
2026-09-22 14:33:53 +0200

    webservice: bump 18.0.2.0.0

--- webservice/__manifest__.py
+++ webservice/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "WebService",
     "summary": """Defines webservice abstract definition to be used generally""",
-    "version": "18.0.1.1.2",
+    "version": "18.0.2.0.0",
     "license": "AGPL-3",
     "development_status": "Production/Stable",
     "maintainers": ["etobella", "simahawk"],
